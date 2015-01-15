4f623c8afa44ad61b27df3da76de8319bdb730ba
Mag <mag@magwas.rulez.org>
2015-01-15 21:12:02 +0100

    first use case for debian proposal

--- spec/features/proposals/proposal_solutons_spec.rb
+++ spec/features/proposals/proposal_solutons_spec.rb
@@ -41,4 +41,22 @@ describe 'create proposal solutions', type: :feature, js: true do
     visit edit_proposal_path(@public_proposal)
     expect(page).to have_content "Unauthorized access"
   end
+
+#  it 'can create solutions in others debian proposal' do
+#    @user = create(:default_user)
+#    @user1 = create(:default_user)
+#    @ability = Ability.new(@user)
+#    @public_proposal = create(:public_proposal, quorum: BestQuorum.public.first, current_user_id: @user.id, proposal_type: ProposalType.find_by_name('DEBIAN'))
+#
+#    login_as @user1, scope: :user
+#
+#    visit edit_proposal_path(@public_proposal)
+#    page_should_be_ok
+#    expect(page).to have_content @public_proposal.title
+#    expect(@ability).to be_able_to(:show, @public_proposal)
+#    expect(@ability).to be_able_to(:participate, @public_proposal)
+#
+#    click_link I18n.t('pages.proposals.edit.add_solution.standard')
+#    expect(page).to have_content "Solution 2"
+#  end
 end
